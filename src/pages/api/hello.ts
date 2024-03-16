4674179840a0a73e3e7b6c55c0b6adc6e0ea7193
Christopher Angelo <angelo@angeloanan.xyz>
2024-03-17 01:56:31 +0700

    feat: api endpoint for mood

--- src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from 'astro'
-
-export const GET: APIRoute = async () => {
-  return new Response(
-    JSON.stringify({
-      hello: 'world!'
-    })
-  )
-}
---
+++ src/pages/api/mood.ts
@@ -0,0 +1,58 @@
+import type { APIRoute } from 'astro'
+import { Mood, db, desc } from 'astro:db'
+import { Type } from '@sinclair/typebox'
+import { TypeCompiler } from '@sinclair/typebox/compiler'
+
+export const prerender = false
+
+const getLatestMoodQuery = db
+  .select({ energy: Mood.energy, pleasantness: Mood.pleasantness, timestamp: Mood.timestamp })
+  .from(Mood)
+  .limit(1)
+  .orderBy(desc(Mood.timestamp))
+  .prepare()
+
+/**
+ * Get the latest mood
+ */
+export const GET: APIRoute = async () => {
+  const mood = await getLatestMoodQuery.execute()
+
+  return new Response(
+    JSON.stringify({
+      timestamp: mood[0]?.timestamp,
+      energy: mood[0]?.energy,
+      pleasantness: mood[0]?.pleasantness
+    })
+  )
+}
+
+const UpdateLatestMoodSchema = Type.Object({
+  password: Type.String(),
+  energy: Type.Number({ minimum: -1, maximum: 1 }),
+  pleasantness: Type.Number({ minimum: -1, maximum: 1 })
+})
+const UpdateLatestMoodValidator = TypeCompiler.Compile(UpdateLatestMoodSchema)
+/**
+ * Update the latest mood
+ */
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json()
+  const bodyValidateError = UpdateLatestMoodValidator.Errors(body).First()
+  if (bodyValidateError) {
+    return new Response(JSON.stringify(bodyValidateError), { status: 400 })
+  }
+  if (body.password !== import.meta.env.ADMIN_PASSWORD) {
+    return new Response('Invalid password', { status: 401 })
+  }
+
+  await db
+    .insert(Mood)
+    .values({
+      energy: body.energy,
+      pleasantness: body.pleasantness
+    })
+    .execute()
+
+  return new Response()
+}
